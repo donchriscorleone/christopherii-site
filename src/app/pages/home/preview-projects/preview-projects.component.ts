ee068425bcfcad514a815d7f85ed53f836e9c4f9
Christopher II Lajom <christopheriilajom@gmail.com>
2022-12-04 16:26:13 +0800

    Added links.

--- src/app/pages/home/preview-projects/preview-projects.component.ts
+++ src/app/pages/home/preview-projects/preview-projects.component.ts
@@ -17,18 +17,32 @@ export class PreviewProjectsComponent implements OnInit {
       imgPath: this.assetPath + "joblisting.png",
     },
     {
-      title: "Job Listing Application",
-      description: "Static data using plain CSS and JS",
-      github: "https://github.com/donchriscorleone/job-listings-with-filtering-solution",
-      liveSite: "https://donchriscorleone.github.io/job-listings-with-filtering-solution/",
-      imgPath: this.assetPath + "joblisting.png",
+      title: "IP Address Tracker",
+      description: "Using Plain JS, CSS, LeafletJS, and Ipify",
+      github: "https://github.com/donchriscorleone/ip-address-tracker-master",
+      liveSite: "https://ip-address-tracker-devchristopherii.netlify.app/",
+      imgPath: this.assetPath + "ipaddresstracker.png",
     },
     {
-      title: "Job Listing Application",
-      description: "Static data using plain CSS and JS",
-      github: "https://github.com/donchriscorleone/job-listings-with-filtering-solution",
-      liveSite: "https://donchriscorleone.github.io/job-listings-with-filtering-solution/",
-      imgPath: this.assetPath + "joblisting.png",
+      title: "URL Shortening Page",
+      description: "Mobile first workflow using CSS and JS",
+      github: "https://github.com/donchriscorleone/shortly-url-shortening-api-challenge-solution",
+      liveSite: "https://donchriscorleone.github.io/shortly-url-shortening-api-challenge-solution/",
+      imgPath: this.assetPath + "urlshortening.png",
+    },
+    {
+      title: "Manage Landing Page",
+      description: "Creating landing page using plain CSS and JS",
+      github: "https://github.com/donchriscorleone/manage-landing-page-solution",
+      liveSite: "https://donchriscorleone.github.io/manage-landing-page-solution/index.html",
+      imgPath: this.assetPath + "managelanding.png",
+    },
+    {
+      title: "Easybank Landing Page",
+      description: "Mobile first approach using plain CSS and JS",
+      github: "https://github.com/donchriscorleone/easybank-landing-page",
+      liveSite: "https://donchriscorleone.github.io/easybank-landing-page/",
+      imgPath: this.assetPath + "easybanklanding.png",
     }
   ]
 
